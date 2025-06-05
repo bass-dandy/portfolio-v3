875fdd3a8efc7f2985ea1169f5004c56d089dad8
bass-dandy <ctdinh93@gmail.com>
2025-06-05 02:34:38 -0400

    remove console

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -3,10 +3,8 @@ import {useEffect} from 'react';
 const useClickOutside = (ref: React.RefObject<HTMLElement | null>, callback: (e: Event) => void) => {
 	useEffect(() => {
 		const onClickOutside = (e: MouseEvent) => {
-			console.log('outer');
 			if (ref.current !== e.target && !ref.current?.contains(e.target as HTMLElement)) {
 				callback(e);
-				console.log('inner');
 			}
 		};
 		window.addEventListener('click', onClickOutside);
